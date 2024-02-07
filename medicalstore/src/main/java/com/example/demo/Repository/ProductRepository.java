e4101f3b72d988812eba6e70b96a205acfed6c7b
AnushyaAsh <142390408+AnushyaAsh@users.noreply.github.com>
2024-02-07 14:46:52 +0530

    Add files via upload

---
+++ medicalstore/src/main/java/com/example/demo/Repository/ProductRepository.java
@@ -0,0 +1,20 @@
+package com.example.demo.Repository;
+
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.Model.Product;
+
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+     
+	Product findByName (String name);
+	@Query("SELECT p FROM Product p WHERE p.name LIKE ?1%")
+	public List<Product> findAll(String keyword);
+}
